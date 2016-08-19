98216def7c1c541c8b0fbfe79161cc9d0b60c430
tek <torstenschmits@gmail.com>
2016-08-19 18:47:10 +0200

    adapt rename of tryp

--- rplugin/python3/myo_nvim.py
+++ rplugin/python3/myo_nvim.py
@@ -1,6 +1,15 @@
+import os
+
 import neovim
 
 from myo.nvim_plugin import MyoNvimPlugin
+
+import amino
+from amino.logging import tryp_file_logging, VERBOSE
+
+if 'MYO_DEBUG' in os.environ:
+    amino.development = True
+    tryp_file_logging(VERBOSE)
 
 
 @neovim.plugin
